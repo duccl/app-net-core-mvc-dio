4a55e15533b44ecfb0fb9f8271e3b24d9caddec6
Coltri <edr.ccol@gmail.com>
2020-12-21 22:39:01 -0300

    Data annotations at Categoria and Produto

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,10 @@
-
+using System.ComponentModel.DataAnnotations;
 namespace app_net_core.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
         public System.Collections.Generic.List<Produto> Produtos { get; set; }
     }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,12 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 namespace app_net_core.Models
 {
     public class Produto
     {
+        [Display(Name = "Descrição Categoria")]
         public Categoria categoria { get; set; }
         public int CategoriaId { get; set; }
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório!")]
         public string Nome { get; set; }
         public int Quantidade { get; set; }
+        [Range(1.00,double.MaxValue, ErrorMessage = "Valor Inválido!")]
         public decimal Preco { get; set; }
     }
 }
